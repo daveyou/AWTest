d7a186e00dcd32c09d2fc1a7fd17034471898d42
David You <david.you@red-gate.com>
2015-10-16 11:50:22 -0700

    10/16 changeset

--- Data/Person.ContactType_Data.sql
+++ Data/Person.ContactType_Data.sql
@@ -1,3 +1,6 @@
 SET IDENTITY_INSERT [Person].[ContactType] ON
+INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (3, N'admin intern', '2015-10-16 11:49:33.723')
+SET IDENTITY_INSERT [Person].[ContactType] OFF
+SET IDENTITY_INSERT [Person].[ContactType] ON
 INSERT INTO [Person].[ContactType] ([ContactTypeID], [Name], [ModifiedDate]) VALUES (1, N'admin assistant', '2015-10-16 09:11:53.057')
 SET IDENTITY_INSERT [Person].[ContactType] OFF
